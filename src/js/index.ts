665031d68b08ec0f175e3b1c585c19adb999ef53
Victor Friis-Jensen <vict5481@edu.easj.dk>
2020-10-23 09:56:40 +0200

    Working Get All

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,23 +3,35 @@ import axios, {
     AxiosError
 } from "../../node_modules/axios/index"
 
+interface IRecord {
+    nr: number
+    title: string
+    artist: string
+    duration: number
+    yearOfPublication: number
+}
+
+let baseUrl: string = "https://restmusicrecords.azurewebsites.net/api/MusicRecords"
 
 
 new Vue({
     el: "#app",
     data: {
-        name: "",
-        greeting: ""
+        records: []
     },
     methods: {
-        sayHello(name: string) {
-            console.log("Say Hello " + name)
-            if (name.length == 0) {
-                this.greeting = "Hello NoName"
-            }
-            else {
-                this.greeting = "Hello " + name
-            }
+        getAllRecords() {
+            this.helperGetAndShow(baseUrl)
+        },
+        helperGetAndShow(url: string) { // helper metode: getAllRecord + getByVendor are very similar
+            axios.get<IRecord[]>(url)
+                .then((response: AxiosResponse<IRecord[]>) => {
+                    this.records = response.data
+                })
+                .catch((error: AxiosError) => {
+                    //this.message = error.message
+                    alert(error.message) // https://www.w3schools.com/js/js_popup.asp
+                })
         }
     }
 })
